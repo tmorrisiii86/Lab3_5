e0e5ca924cc9fa47011c7bdd276e05131ceab256
=tmorrisiii86 <=tmorrisiii86@gmail.com>
2020-02-23 09:43:32 -0600

    Add project files.

---
+++ Lab3_5_Thomas_Morris/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab3_5_Thomas_Morris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "viagra";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("This message is spam.");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This message is not spam.");
+            }
+        }
+    }
+}
